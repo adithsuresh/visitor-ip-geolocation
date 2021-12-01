67ede33c217784d43c38d56087ede7975fb5dfea
adithsuresh <adith7sj@gmail.com>
2021-12-01 17:46:37 +0530

    added option to export and some additional fields

--- ipgeo.js
+++ ipgeo.js
@@ -75,6 +75,13 @@ async function getGeoIpDetails() {
     if (ipAddress) {
       geoipData.ip = ipAddress;
     }
+    geoipData = {
+      ip: geoipData.ip,
+      operating_system: `${client.os} ${client.osVersion}`,
+      browser: `${client.browser} ${client.browserMajorVersion}`,
+      mobile: client.mobile,
+      ...geoipData,
+    };
     return geoipData;
   } catch (err) {
     console.log(err);
@@ -126,6 +133,31 @@ function createGeoIpTable(data) {
   } catch (err) {
     console.log(err);
   }
+}
+
+function tableToCSV(table, filename) {
+  var data = [];
+  var rows = document.querySelectorAll(`${table} tr`);
+  for (var i = 0; i < rows.length; i++) {
+    var row = [],
+      cols = rows[i].querySelectorAll("td, th");
+    for (var j = 0; j < cols.length; j++) {
+      row.push(cols[j].innerText);
+    }
+    data.push(row.join(","));
+  }
+  downloadCSV(data.join("\n"), filename);
+}
+
+function downloadCSV(csv, filename) {
+  var csv_file, download_link;
+  csv_file = new Blob([csv], { type: "text/csv" });
+  download_link = document.createElement("a");
+  download_link.download = filename;
+  download_link.href = window.URL.createObjectURL(csv_file);
+  download_link.style.display = "none";
+  document.body.appendChild(download_link);
+  download_link.click();
 }
 
 function capitalizeAll(str) {
